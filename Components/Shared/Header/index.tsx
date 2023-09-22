09a7441bf40dd5cc8fbeadeb977b54d91304d455
satyam <satyamdubey9450@gmial.com>
2023-09-22 19:28:18 +0530

    Ui update
    Ui update

--- Components/Shared/Header/index.tsx
+++ Components/Shared/Header/index.tsx
@@ -12,6 +12,7 @@ import wats from "@/public/Assets/pages/whats.png";
 import downArrow from "@/public/Assets/pages/down-arrow.png";
 import CompanyDetails from "@/Constent/CompanyDetails";
 import CommanSite from "@/Constent/CommanSite";
+import { useRouter } from "next/router";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -22,7 +23,7 @@ const Header = () => {
       setScroll(window.scrollY > 10)
     })
   }, [])
-
+  const router = useRouter();
   return (
     <div className="outerMainInfoHeader">
       <header className="headerMain">
@@ -35,14 +36,26 @@ const Header = () => {
                 </Link>
               </div>
               <div className="menuRight desktop">
-                  <Link href="/" className="active">
+                  <span className={router.asPath == "/" ? "active" : ""}>
+                  <Link href="/">
                     Home
                   </Link>
+                  </span>
+                  <span className={router.asPath == "/courses" ? "active" : ""}>
                   <Link href="/courses">Courses</Link>
+                  </span>
+                  <span className={router.asPath == "/surteerth" ? "active" : ""}>
                   <Link href="/surteerth">Surteerth Academy</Link>
+                  </span>
+                  <span className={router.asPath == "/masterClass" ? "active" : ""}>
                   <Link href="/masterClass">Masterclass</Link>
+                  </span>
+                  <span className={router.asPath == "/about" ? "active" : ""}>
                   <Link href="/about">About Us</Link>
+                  </span>
+                  <span className={router.asPath == "/contact" ? "active" : ""}>
                   <Link href="/contact">Contact Us</Link>
+                  </span>
               </div>
               <div className="socialIconInfo">
                 <Link href={"/"}>
